9da7c2430fcf094c065b89c4e5ec7d1ed1cf1b66
clariserni <clarissaserni.96@gmail.com>
2024-06-25 21:43:09 +0200

    late night update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,56 @@
+import java.util.List;
+
 public class App {
+
+      /**
+     * TODO:
+     * 
+     * completare la classe DataHelper
+     * 
+     * aggiungere i seguenti metodi per testare la classe DataHelper:
+     * - testRandomInteger : void
+     * testare le 3 implementazioni del metodo getRandomInteger
+     * 
+     * - testRandomUniqueInteger : void
+     * testare le 3 implementazioni del metodo getRandomUniqueInteger
+     * 
+     * - testFrequencyMap : void
+     * testare il metodo getFrequencyMap;
+     * sfruttare i metodi precedentemente svolti e utilizzarli come
+     * dati in ingresso per il metodo getFrequencyMap
+     * 
+     */
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //testRandomInteger();
+        //testRandomUniqueInteger();
+        testFrequencyMap();
+    }
+
+    public static void testRandomInteger(){
+        DataHelper dh = new DataHelper();
+
+        System.out.println("Size: "+dh.getRandomInteger(6));
+        System.out.println("Size, max: "+dh.getRandomInteger(5, 10));
+        System.out.println("Size, min, max: "+dh.getRandomInteger(6,3,8));
+
+    }
+
+    public static void testRandomUniqueInteger(){
+        DataHelper dh = new DataHelper();
+
+        System.out.println("Size: "+dh.getRandomUniqueInteger(8));
+        System.out.println("Size, max: "+dh.getRandomUniqueInteger(7,10));
+        System.out.println("Size, min, max: "+dh.getRandomUniqueInteger(10,4,20));
+    }
+
+    public static void testFrequencyMap(){
+        DataHelper dh = new DataHelper();
+
+        List<Integer> lista = dh.getRandomInteger(10, 8);
+        
+        System.out.println("Lista: "+lista);
+        System.out.println("Frequency map: "+dh.getFrequencyMap(lista)); 
     }
 }
--- src/DataHelper.java
+++ src/DataHelper.java
@@ -1,3 +1,127 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
 public class DataHelper {
 
+     // TODO: completare i metodi seguenti
+
+    // restituisce una lista di numeri interi casuali compresi tra 0 e 10
+    public static List<Integer> getRandomInteger(int size) {
+
+        Random random = new Random();
+
+        List<Integer> numeriRandom = new ArrayList<>();
+
+        for (int i=0; i<size; i++){
+            int numeroRandom = random.nextInt(11);
+            numeriRandom.add(numeroRandom);
+        }
+    
+        return numeriRandom;
+    }
+
+    // restituisce una lista di numeri interi casuali compresi tra 0 e max
+    public static List<Integer> getRandomInteger(int size, int max) {
+        Random random = new Random();
+
+        List<Integer> numeriRandom = new ArrayList<>();
+
+        for (int i=0; i<size; i++){
+            int numeroRandom = random.nextInt(max+1);
+            numeriRandom.add(numeroRandom);
+        }
+    
+        return numeriRandom;
+    }
+
+    // restituisce una lista di numeri interi casuali compresi tra min e max
+    public static List<Integer> getRandomInteger(int size, int min, int max) {
+        Random random = new Random();
+
+        List<Integer> numeriRandom = new ArrayList<>();
+
+        for (int i=0; i<size; i++){
+            int numeroRandom = random.nextInt(max-min+1) + min;
+            numeriRandom.add(numeroRandom);
+        }
+    
+        return numeriRandom;
+        
+    }
+
+    // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e 10
+        public static Set<Integer> getRandomUniqueInteger(int size) {
+            Random random = new Random();
+
+            Set<Integer> nonDuplicati = new HashSet<>();
+
+            if(size <=0 || size>11){
+                System.out.println("Numero non consentito");
+
+            } else{
+            while(nonDuplicati.size()<size){
+                int numeroRandom = random.nextInt(11);
+                nonDuplicati.add(numeroRandom);
+                }
+            }
+            return nonDuplicati;
+        }
+
+        // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e max
+        public static Set<Integer> getRandomUniqueInteger(int size, int max) { 
+            Random random = new Random();
+
+            Set<Integer> nonDuplicati = new HashSet<>();
+
+            if(size <=0 || size>max+1){
+                System.out.println("Numero non consentito");
+
+            } else{
+            while(nonDuplicati.size()<size){
+                int numeroRandom = random.nextInt(max+1);
+                nonDuplicati.add(numeroRandom);
+                }
+            }
+            return nonDuplicati;
+        }
+
+    // restituisce una lista di numeri casuali NON DUPLICATI compresi tra min e max
+        public static Set<Integer> getRandomUniqueInteger(int size, int min, int max) {
+            Random random = new Random();
+
+            Set<Integer> nonDuplicati = new HashSet<>();
+
+            if(size <=0 || size>(max+1)-min){
+                System.out.println("Numero non consentito");
+
+            } else{
+            while(nonDuplicati.size()<size){
+                int numeroRandom = random.nextInt((max+1)-min) + min;
+                nonDuplicati.add(numeroRandom);
+                }
+            }
+            return nonDuplicati;
+
+        }
+
+   // restituisce una mappa di frequenza di numeri interi
+        public static Map<Integer, Integer> getFrequencyMap(List<Integer> list) {
+            Map <Integer, Integer> counter = new HashMap<>();
+
+            for (int i : list){
+                if(counter.containsKey(i)){
+                    int valore = counter.get(i);
+                    counter.put(i, valore+1);
+                }else{
+                    counter.put(i,1);
+                }
+            }
+
+        return counter;
+        }
 }
